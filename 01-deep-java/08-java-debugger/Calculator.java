51ba0f9c74259336ccd5db1e24f507110828a5cb
ronald.pacheco <ronald.pachecoalarcon@dachser.com>
2026-04-22 14:54:54 +0200

    update README and Calculator class for improved argument handling and debugging instructions

--- 01-deep-java/08-java-debugger/Calculator.java
+++ 01-deep-java/08-java-debugger/Calculator.java
@@ -1,9 +1,14 @@
-
 public class Calculator {
 
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 40;
+
+        if (args != null && args.length == 2) {
+            num1 = Integer.parseInt(args[0]);
+            num2 = Integer.parseInt(args[1]);
+        }
+
         System.out.println("Init calculator -> sum " + num1 + " + " + num2);
         int result = sum(num1, num2);
         System.out.println("Result is: " + result);
